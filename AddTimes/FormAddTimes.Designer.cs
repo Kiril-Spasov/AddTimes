eb0a0f05e190a39a5f4d3136c988c93960f8cea8
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-21 11:22:15 +0200

    Add project files.

---
+++ AddTimes/FormAddTimes.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace AddTimes
+{
+    partial class FormAddTimes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAddTimes = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnAddTimes
+            // 
+            this.BtnAddTimes.Location = new System.Drawing.Point(60, 71);
+            this.BtnAddTimes.Name = "BtnAddTimes";
+            this.BtnAddTimes.Size = new System.Drawing.Size(195, 56);
+            this.BtnAddTimes.TabIndex = 0;
+            this.BtnAddTimes.Text = "Add Times";
+            this.BtnAddTimes.UseVisualStyleBackColor = true;
+            this.BtnAddTimes.Click += new System.EventHandler(this.BtnAddTimes_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(359, 71);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(355, 335);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormAddTimes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnAddTimes);
+            this.Name = "FormAddTimes";
+            this.Text = "Add Times";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAddTimes;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ AddTimes/FormAddTimes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AddTimes
+{
+    public partial class FormAddTimes : Form
+    {
+        public FormAddTimes()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddTimes_Click(object sender, EventArgs e)
+        {
+            int day1 = 0, day2 = 0;
+            int hours1 = 0, hours2 = 0;
+            int mins1 = 0, mins2 = 0;
+
+            int minsToDisplay = 0;
+            int hoursToDisplay = 0;
+
+            int totalDays = 0, totalHours = 0, totalMins = 0;
+
+
+            string path = Application.StartupPath + @"\times.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            int pairsCount = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 1; i <= pairsCount; i++)
+            {
+                day1 = Convert.ToInt32(streamReader.ReadLine());
+                hours1 = Convert.ToInt32(streamReader.ReadLine());
+                mins1 = Convert.ToInt32(streamReader.ReadLine());
+
+                day2 = Convert.ToInt32(streamReader.ReadLine());
+                hours2 = Convert.ToInt32(streamReader.ReadLine());
+                mins2 = Convert.ToInt32(streamReader.ReadLine());
+
+
+                totalMins = ConvertDaysToMins(day1 + day2) + ConvertHoursToMins(hours1 + hours2) + mins1 + mins2;
+
+                //There are 1440 mins in 1 day.
+                totalDays = totalMins / 1440;
+
+                //There are 60 mins in 1h.
+                totalHours = totalMins / 60;
+
+                hoursToDisplay = totalHours % 24;
+                minsToDisplay = totalMins % 60;
+
+
+                TxtResult.Text += day1 + " days " + hours1 + " hours " + mins1 + " mins " + " + " +
+                         day2 + " days " + hours2 + " hours " + mins2 + " mins " + Environment.NewLine;
+
+                TxtResult.Text += "equals" + Environment.NewLine;
+
+                TxtResult.Text += totalDays + " days " + hoursToDisplay + " hours " + minsToDisplay + " mins " + Environment.NewLine;
+
+                TxtResult.Text += Environment.NewLine;
+            }      
+        }
+
+        private int ConvertDaysToMins(int days)
+        {
+            int mins = days * 24 * 60;
+
+            return mins;
+        }
+
+        private int ConvertHoursToMins(int hours)
+        {
+            int mins = hours * 60;
+
+            return mins;
+        }
+
+    }
+}
